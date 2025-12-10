759edbe63096fa2076883c3dac646cfa7b82ba80
AkashGaneshan-dev <akashganeshaner@gmail.com>
2025-12-11 01:58:35 +0530

    Vercel port updated

--- jeevanax_website/src/server.ts
+++ jeevanax_website/src/server.ts
@@ -1,7 +1,8 @@
 import app from "./app";
 
 const PORT = process.env.PORT || 3000;
+const VERCEL_PORT = process.env.VERCEL_PORT
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(VERCEL_PORT, () => {
+    console.log(`Server running on port ${VERCEL_PORT}`);
 });
